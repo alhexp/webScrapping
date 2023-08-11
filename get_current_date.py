e7ce5899af57671661c2d3b4f45b9f996595f267
alhexp <alhexp@users.noreply.github.com>
2023-08-11 14:24:55 +0000

    deploy: 7c7d3488003ea2c2cf5f87efd5770fe629643149

--- get_current_date.py
+++ get_current_date.py
@@ -10,16 +10,14 @@ def main():
     str = f"Current Date: {current_date}" 
     
     try:
-        with open(archivo, 'r'):
+        with open(archivo, 'a') as file:
+            file.write(current_date + '\n')
             existe = True
     except FileNotFoundError:
         pass
 
     if not existe:
         with open(archivo, 'w') as file:
-            file.write(current_date + '\n')
-    else:
-        with open(archivo, 'a') as file:
             file.write(current_date + '\n')
 
 if __name__ == "__main__":
