056a3b133aecce3df29344e51e1a2625cc391d8d
alhexp <pc0files1@gmail.com>
2023-09-02 19:54:38 -0400

    working with HTML file from Python

--- get_current_date.py
+++ get_current_date.py
@@ -11,6 +11,8 @@ def main():
     print(os.getcwd())
     print(os.listdir(dir))
     print("hello world")
+    ht = os.chdir(dir + '\public')
+    print(os.getcwd())
 
     current_date = datetime.datetime.now().strftime("%Y-%m-%d")    
     str = f"Current Date: {current_date}"     
@@ -25,6 +27,12 @@ def main():
     if not existe:
         with open(archivo, 'w') as file:
             file.write(str + '\n')
+
+    try:
+        with open(ht, 'w', encoding='utf-8') as html:
+            html.replace('Current Date', f'Current Date\n%str', count=2)
+    except FileNotFoundError:
+        pass
 
 if __name__ == "__main__":
     main()
