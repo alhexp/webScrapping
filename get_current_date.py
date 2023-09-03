fb55b55f1b85d27013569d3fe92c63b505a02895
alhexp <pc0files1@gmail.com>
2023-09-02 20:42:26 -0400

    text from find method deprecated, using string

--- get_current_date.py
+++ get_current_date.py
@@ -29,7 +29,7 @@ def main():
         with open(html_file, 'r', encoding='utf-8') as html:
             content = html.read()
         soup = BeautifulSoup(content, 'html.parser')
-        patron = soup.find('h1', text='Current Date')
+        patron = soup.find('h1', string='Current Date')
         if patron:
             patron.string = f'Current Date\n%str'
         with open(html_file, 'w', encoding='utf-8') as html:
