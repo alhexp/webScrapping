d4c6cdef96e1cecfd55e3ee37728ee6f9351e943
alhexp <pc0files1@gmail.com>
2023-09-06 12:32:14 -0400

    goal achieved, implementing the web scrapper

--- get_current_date.py
+++ get_current_date.py
@@ -3,15 +3,24 @@
 import datetime, requests, os, re
 from bs4 import BeautifulSoup
 
+def escape(s, quote=None):
+    '''Replace special characters "&", "<" and ">" to HTML-safe sequences.
+    If the optional flag quote is true, the quotation mark character (")
+    is also translated.'''
+    s = s.replace("&", "&amp;") # Must be done first!
+    s = s.replace("<", "&lt;")
+    s = s.replace(">", "&gt;")
+    if quote:
+        s = s.replace('"', "&quot;")
+    return s
+
 def main():
     dir = os.getcwd()
     archivo = dir + "\\" + 'current_date.txt'
     existe = False
-    print(os.getcwd())
     html_file = dir + '/public/index.html'
-    print(html_file)
 
-    current_date = datetime.datetime.now().strftime("%Y-%m-%d")    
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")    
     str = f"Current Date: {current_date}"     
 
     try:
@@ -24,18 +33,20 @@ def main():
     if not existe:
         with open(archivo, 'w') as file:
             file.write(str + '\n')
+    else:
+        try:        
+            with open(html_file, 'r') as html:
+                content = html.read()
+                soup = BeautifulSoup(content, 'lxml')
+                patron = soup.find('h1')                
+                if patron:
+                    patron.string = f'{str}'
+                    print(patron)
+            with open(html_file, 'w') as html:
 
-    try:        
-        with open(html_file, 'r') as html:
-            content = html.read()
-        soup = BeautifulSoup(content, 'html.parser')
-        patron = soup.find('h1', string='Current Date')
-        if patron:
-            patron.string = f'Current Date\n{str}'
-        with open(html_file, 'w') as html:
-            html.write(soup.prettify())
-    except FileNotFoundError:
-        pass
+                html.write(soup.prettify())                
+        except FileNotFoundError:
+            pass
 
 if __name__ == "__main__":
     main()
